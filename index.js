dbbf99a5f938d5b5567ac63f2c6a417710e7c1d3
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-08-09 23:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, ';
-const excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
